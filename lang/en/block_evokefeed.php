8b6f46637c1c0effae7e1163c8a5863503c442f5
John Moravec <51122406+educationfutures@users.noreply.github.com>
2022-03-23 09:00:20 -0500

    Update block_evokefeed.php

--- lang/en/block_evokefeed.php
+++ lang/en/block_evokefeed.php
@@ -15,7 +15,7 @@ $string['pluginname'] = 'Network Activity';
 $string['privacy:metadata'] = 'Network Activity Feed does not store any personal data';
 
 $string['portfolio_comment_string'] = '{$a} commented on a post';
-$string['portfolio_like_string'] = '{$a} liked an evidence';
+$string['portfolio_like_string'] = '{$a} liked a portfolio post';
 $string['portfolio_earnedpoints_string'] = 'Earned {$a} points';
 $string['portfolio_earnedevocoins_string'] = 'Earned {$a} EVCs';
 $string['portfolio_earnedbadge_string'] = 'Earned a badge';
