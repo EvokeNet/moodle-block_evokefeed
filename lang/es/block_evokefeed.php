52484de2d9c71351827a2813e0d560029bbb2d12
John Moravec <51122406+educationfutures@users.noreply.github.com>
2022-03-25 08:15:01 -0500

    Create block_evokefeed.php

---
+++ lang/es/block_evokefeed.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_evokefeed
+ * @category    string
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Actividad de la red';
+$string['privacy:metadata'] = 'The Network Activity Feed does not store any personal data';
+
+$string['portfolio_comment_string'] = '{$a} comentó en un post';
+$string['portfolio_like_string'] = '{$a} le gustó una evidencia';
+$string['portfolio_earnedpoints_string'] = 'Ganó {$a} puntos';
+$string['portfolio_earnedevocoins_string'] = 'Ganó {$a} EVCs';
+$string['portfolio_earnedbadge_string'] = 'Earned a badge';
