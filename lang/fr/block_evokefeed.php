0ddc9f043783077ba5d324f528b8567eafa59c2b
Willian Mano <willian.mano@jove.com>
2023-03-31 11:54:43 -0300

    Add fr lang

---
+++ lang/fr/block_evokefeed.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     block_evokefeed
+ * @category    string
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Activité de réseau';
+$string['privacy:metadata'] = 'Le fil d\'activité de réseau ne stocke aucune donnée personnelle';
+
+$string['you'] = 'Vous';
+
+$string['portfolio_commentedyourpost_string'] = 'a commenté votre publication';
+$string['portfolio_commentedapost_string'] = 'a commenté une publication';
+
+$string['portfolio_likedyourpost_string'] = 'a aimé votre publication de portfolio';
+$string['portfolio_likedapost_string'] = 'a aimé une publication de portfolio';
+
+$string['portfolio_earnedpoints_string'] = 'a obtenu {$a} points';
+$string['portfolio_earnedevocoins_string'] = 'a obtenu {$a} EVCs';
+
+$string['badge_earnedbadge_string'] = 'a obtenu un badge';
+
+$string['portfolio_submission_string'] = 'a soumis une évaluation';
